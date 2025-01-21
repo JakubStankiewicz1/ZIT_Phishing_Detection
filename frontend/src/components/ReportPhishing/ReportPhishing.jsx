480aa6c74d8f7eb051c263001a533cfb4fe477ac
JakubStankiewicz1 <272264@student.pwr.edu.pl>
2025-01-22 00:05:50 +0100

    ReportPhishing styling

--- frontend/src/components/ReportPhishing/ReportPhishing.jsx
+++ frontend/src/components/ReportPhishing/ReportPhishing.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 import "./reportPhishing.css";
 import video from "../../assets/reportPhishing.gif";
 
 const ReportPhishing = () => {
   return (
-    <div className="phishing-card">
-      <img src={video} alt="" />
-      <h3>Co robić, gdy dostaniesz mail phishingowy?</h3>
-      <p>
-        Jeśli otrzymasz podejrzany e-mail, nie klikaj w żadne linki ani nie pobieraj załączników. 
-        Zgłoś ten e-mail na stronie:
-      </p>
+    <div className="phishing">
+      <div className="phishingCart">
+        <div className="phishingCartText">
+          <h3 className="phishingCartTextTitle">Co robić, gdy dostaniesz mail phishingowy?</h3>
+          <p className="phishingCartTextEle">
+            Jeśli otrzymasz podejrzany e-mail, nie klikaj w żadne linki ani nie pobieraj załączników.
+            <br />
+            <br />
+            Zgłoś ten e-mail na stronie:{" "}
+            <a
+              href="https://incydent.cert.pl/#!/lang=pl,entityType=notObligatedEntity,easyIncidentType=email"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="websiteUrl"
+            >
+              incydent.cert.pl
+            </a>
+          </p>
+        </div>
+
+        <img src={video} alt="" className="phishingCartImg" />
+      </div>
     </div>
   );
 };
