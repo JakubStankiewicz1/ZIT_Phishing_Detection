c0d99522934127dc7fc025259ad1cc59362cffb8
JakubStankiewicz1 <272264@student.pwr.edu.pl>
2025-01-22 00:24:38 +0100

    Update ReportRphishing page

--- frontend/src/components/ReportPhishing/ReportPhishing.jsx
+++ frontend/src/components/ReportPhishing/ReportPhishing.jsx
@@ -7,7 +7,9 @@ const ReportPhishing = () => {
     <div className="phishing">
       <div className="phishingCart">
         <div className="phishingCartText">
-          <h3 className="phishingCartTextTitle">Co robić, gdy dostaniesz mail phishingowy?</h3>
+          <h3 className="phishingCartTextTitle">
+            Co robić, gdy dostaniesz mail phishingowy?
+          </h3>
           <p className="phishingCartTextEle">
             Jeśli otrzymasz podejrzany e-mail, nie klikaj w żadne linki ani nie pobieraj załączników.
             <br />
@@ -22,9 +24,25 @@ const ReportPhishing = () => {
               incydent.cert.pl
             </a>
           </p>
+          <p className="phishingCartTextEle">
+            <strong>Jeśli kliknąłeś w link z podejrzanej wiadomości e-mail i wprowadziłeś swoje dane logowania na fałszywej stronie:</strong>
+            <ul>
+              <li>Natychmiast zmień swoje hasła do wszystkich powiązanych kont.</li>
+              <li>Włącz uwierzytelnianie dwuskładnikowe (2FA), jeśli to możliwe.</li>
+            </ul>
+          </p>
+          <p className="phishingCartTextEle">
+            <strong>Jeśli wysłałeś komuś pieniądze:</strong>
+            <ul>
+              <li>Niezwłocznie skontaktuj się z bankiem i zgłoś incydent.</li>
+              <li>Rozważ zgłoszenie sprawy na policję.</li>
+            </ul>
+          </p>
+          <p className="phishingCartTextEle">
+            Działaj szybko, ale nie poddawaj się panice – każdy krok zwiększa Twoje szanse na zminimalizowanie skutków ataku.
+          </p>
         </div>
-
-        <img src={video} alt="" className="phishingCartImg" />
+        <img src={video} alt="Phishing animation" className="phishingCartImg" />
       </div>
     </div>
   );
